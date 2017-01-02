cc2cc3b0b1dba6bd62222456998417b2f7f40112
lzj <827579668@qq.com>
2017-01-02 22:39:00 +0800

    Initial commit

---
+++ app/src/main/java/s07150629/gdmec/edu/cn/work1/MainActivity.java
@@ -0,0 +1,13 @@
+package s07150629.gdmec.edu.cn.work1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
